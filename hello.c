b32b1cb4e1376423ad864d9c5621eff63e69a76a
feecle6418 <feecle6418@outlook.com>
2025-05-14 18:34:49 +0000

    try

--- hello.c
+++ hello.c
@@ -1,7 +1,31 @@
-int main() {
-  // 我是注释诶嘿嘿
-  /* this is a multi
-  line comment/*
-  */
-  return 0x123;
+/*/skipher                          skipher/*/
+//int          main(           )             {
+int        main( // ){return 0101;}
+)                {
+	///       /return 0123;}            /*
+	/*
+	
+
+
+
+
+
+
+
+	//}return 021;*/
+	//return 0221;*//*
+	/*/*************///return 0333;
+
+//*/*//*/*return 0;
+/***//***/return 01;
+/*/**///*/return 010;
+/**//*/***return 011;
+*//*/**//*return 0100;
+*//*/*****return 0101;
+**//***//*return 0110;
+*//*////*/return 0111;
+///*///***return 01000;
+/*///*///*return 01001;
+
+	//*/
 }
