a5b88c9dbeef795f0dd628a6cb61f259295ac34b
feecle6418 <feecle6418@outlook.com>
2025-06-02 14:11:29 +0000

    loadstore optimize

--- hello.c
+++ hello.c
@@ -1,67 +1,66 @@
-int set(int a[], int pos, int d){
-    const int bitcount = 30;
-    int x[bitcount + 1] = {};
-
-    x[0] = 1;
-    x[1] = x[0] * 2;
-    x[2] = x[1] * 2;
-    x[3] = x[2] * 2;
-    x[4] = x[3] * 2;
-    x[5] = x[4] * 2;
-    x[6] = x[5] * 2;
-    x[7] = x[6] * 2;
-    x[8] = x[7] * 2;
-    x[9] = x[8] * 2;
-    x[10] = x[9] * 2;
-    
-    int i = 10;
-    while (i < bitcount){
-        i = i + 1;
-        x[i] = x[i - 1] * 2;
+int n;
+int QuickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int i;
+        i = low;
+        int j;
+        j = high;
+        int k;
+        k = arr[low];
+        while (i < j)
+        {
+            while(i < j && arr[j] > k - 1)
+            {
+                j = j - 1;
+            }
+ 
+            if(i < j)
+            {
+                arr[i] = arr[j];
+                i = i + 1;
+            }
+ 
+            while(i < j && arr[i] < k)
+            {
+                i = i + 1;
+            }
+ 
+            if(i < j)
+            {
+                arr[j] = arr[i];
+                j = j - 1;
+            }
+        }
+ 
+        arr[i] = k;
+        int tmp;
+        tmp = i - 1;
+        tmp = QuickSort(arr, low, tmp);
+        tmp = i + 1;
+        tmp = QuickSort(arr, tmp, high);
     }
-
-    int v = 0;
-
-    if (pos / bitcount >= 10000) return 0;
-    
-    if (a[pos / bitcount] / (x[pos % bitcount]) % 2 != d){
-        if (a[pos / bitcount] / (x[pos % bitcount]) % 2 == 0)
-            if (d == 1)
-                v = x[pos % bitcount];
-        
-        if (a[pos / bitcount] / x[pos % bitcount] % 2 == 1)
-            if (d == 0)
-                v = v - x[pos % bitcount];
-    }
-
-    a[pos / bitcount] = a[pos / bitcount] + v;
     return 0;
 }
 
-int seed[3] = {19971231, 19981013, 1000000000 + 7};
-int staticvalue = 0;
-
-int rand(){
-    staticvalue = staticvalue * seed[0] + seed[1];
-    staticvalue = staticvalue % seed[2];
-    if (staticvalue < 0) staticvalue = seed[2] + staticvalue;
-    return staticvalue;
-}
-
-int a[10000] = {};
 int main(){
-    
-    int n = getint();
-    staticvalue = getint();
-    starttime();
-    int x, y;
-    while (n > 0){
-        n = n - 1;
-        x = rand() % 300000;
-        y = rand() % 2;
-        set(a, x, y);
+    n = 10;
+    int a[10];
+    a[0]=4;a[1]=3;a[2]=9;a[3]=2;a[4]=0;
+    a[5]=1;a[6]=6;a[7]=5;a[8]=7;a[9]=8;
+    int i;
+    i = 0;
+    int tmp;
+    tmp = 9;
+    i = QuickSort(a, i, tmp);
+    while (i < n) {
+        int tmp;
+        tmp = a[i];
+        putint(tmp);
+        tmp = 10;
+        putch(tmp);
+        i = i + 1;
     }
-    stoptime();
-    putarray(10000, a);
     return 0;
 }
--- src/optimize_loadstore.rs
+++ src/optimize_loadstore.rs
@@ -19,10 +19,13 @@ impl OptimizeLoadStore for Program {
     }
 }
 
+// TODO: 1. 可达定值分析；2. 特判不能把函数的参数直接用。
+
 impl OptimizeLoadStore for FunctionData {
     fn optimize_loadstore(&mut self) {
         let mut bbids: Vec<BasicBlock> = Vec::new();
         let mut bbs: Vec<BasicBlockVal> = Vec::new();
+        let mut paramlist: HashMap<Value, bool> = HashMap::new();
         for (&bb, _) in self.layout_mut().bbs_mut().iter() {
             bbids.push(bb);
         }
@@ -38,131 +41,151 @@ impl OptimizeLoadStore for FunctionData {
             bbs.push(t);
         }
 
+        for par in self.params() {
+            paramlist.insert(par.clone(), true);
+        }
+
         for blockval in bbs.iter() {
             let mut stored: HashMap<Value, Value> = HashMap::new();
+            let mut replacing: Vec<(Value, Value)> = Vec::new();
             for inst in blockval.insts.iter() {
                 let kind = self.dfg_mut().value(inst.clone()).kind().clone();
                 match kind {
                     ValueKind::Store(store) => {
-                        let src = store.value();
-                        let dst = store.dest();
+                        let mut src = store.value();
+                        let mut dst = store.dest();
+                        if paramlist.get(&src).is_some() {
+                            continue;
+                        }
                         stored.insert(dst.clone(), src.clone()); // stored[dst] = src
                     }
                     ValueKind::Load(load) => {
-                        let src = load.src();
+                        let mut src = load.src();
                         let res = stored.get(&src);
                         if !res.is_none() {
-                            let replacer = res.unwrap();
-                            /*let replacerdata = self.dfg_mut().value(replacer.clone());
-                            self.dfg_mut()
-                                .replace_value_with(inst.clone())
-                                .insert_value(replacerdata);*/
-                            let usedby = self.dfg().value(inst.clone()).used_by().clone();
-                            for user in &usedby {
-                                match self.dfg().value(user.clone()).kind().clone() {
-                                    ValueKind::Binary(bin) => {
-                                        let op = bin.op();
-                                        let mut lhs = bin.lhs();
-                                        let mut rhs = bin.rhs();
-                                        if self.dfg().value_eq(lhs, inst.clone()) {
-                                            lhs = replacer.clone();
-                                        }
-                                        if self.dfg().value_eq(rhs, inst.clone()) {
-                                            rhs = replacer.clone();
-                                        }
-                                        self.dfg_mut()
-                                            .replace_value_with(user.clone())
-                                            .binary(op, lhs, rhs);
-                                    }
-                                    ValueKind::Branch(br) => {
-                                        let bbf = br.false_bb();
-                                        let bbt = br.true_bb();
-                                        self.dfg_mut().replace_value_with(user.clone()).branch(
-                                            replacer.clone(),
-                                            bbt,
-                                            bbf,
-                                        );
-                                    }
-                                    ValueKind::Call(ca) => {
-                                        let args = ca.args();
-                                        let callee = ca.callee();
-                                        let vec = args
-                                            .iter()
-                                            .map(|v| {
-                                                if self.dfg().value_eq(v.clone(), inst.clone()) {
-                                                    replacer.clone()
-                                                } else {
-                                                    v.clone()
-                                                }
-                                            })
-                                            .collect();
-                                        self.dfg_mut()
-                                            .replace_value_with(user.clone())
-                                            .call(callee, vec);
-                                    }
-                                    ValueKind::GetElemPtr(va) => {
-                                        let mut lhs = va.src();
-                                        let mut rhs = va.index();
-                                        if self.dfg().value_eq(lhs, inst.clone()) {
-                                            lhs = replacer.clone();
-                                        }
-                                        if self.dfg().value_eq(rhs, inst.clone()) {
-                                            rhs = replacer.clone();
-                                        }
-                                        self.dfg_mut()
-                                            .replace_value_with(user.clone())
-                                            .get_elem_ptr(lhs, rhs);
-                                    }
-                                    ValueKind::GetPtr(va) => {
-                                        let mut lhs = va.src();
-                                        let mut rhs = va.index();
-                                        if self.dfg().value_eq(lhs, inst.clone()) {
-                                            lhs = replacer.clone();
-                                        }
-                                        if self.dfg().value_eq(rhs, inst.clone()) {
-                                            rhs = replacer.clone();
-                                        }
-                                        self.dfg_mut()
-                                            .replace_value_with(user.clone())
-                                            .get_elem_ptr(lhs, rhs);
-                                    }
-                                    ValueKind::Load(_) => {
-                                        self.dfg_mut()
-                                            .replace_value_with(user.clone())
-                                            .load(replacer.clone());
-                                    }
-                                    ValueKind::Return(_) => {
-                                        self.dfg_mut()
-                                            .replace_value_with(user.clone())
-                                            .ret(Some(replacer.clone()));
-                                    }
-                                    ValueKind::Store(st) => {
-                                        let mut lhs = st.value();
-                                        let mut rhs = st.dest();
-                                        if self.dfg().value_eq(lhs, inst.clone()) {
-                                            lhs = replacer.clone();
-                                        }
-                                        if self.dfg().value_eq(rhs, inst.clone()) {
-                                            rhs = replacer.clone();
-                                        }
-                                        self.dfg_mut()
-                                            .replace_value_with(user.clone())
-                                            .store(lhs, rhs);
-                                    }
-                                    _ => {}
-                                }
-                            }
-                            let len = self.dfg().value(inst.clone()).used_by().len();
-                            if len == 0 {
-                                self.layout_mut()
-                                    .bb_mut(blockval.bb)
-                                    .insts_mut()
-                                    .remove(inst);
-                                let _ = self.dfg_mut().remove_value(inst.clone());
-                            }
+                            replacing.push((inst.clone(), res.unwrap().clone()));
+                        } else {
+                            // println!("found sth to optimize");
+                            stored.insert(src.clone(), inst.clone());
                         }
                     }
                     _ => {}
+                }
+            }
+            for (inst, replacer) in replacing.iter().rev() {
+                let usedby = self.dfg().value(inst.clone()).used_by().clone();
+                for user in &usedby {
+                    let res;
+                    match self.dfg().value(user.clone()).kind().clone() {
+                        ValueKind::Binary(bin) => {
+                            let op = bin.op();
+                            let mut lhs = bin.lhs();
+                            let mut rhs = bin.rhs();
+                            if lhs == inst.clone() {
+                                lhs = replacer.clone();
+                            }
+                            if rhs == inst.clone() {
+                                rhs = replacer.clone();
+                            }
+                            res = self
+                                .dfg_mut()
+                                .replace_value_with(user.clone())
+                                .binary(op, lhs, rhs);
+                        }
+                        ValueKind::Branch(br) => {
+                            let bbf = br.false_bb();
+                            let bbt = br.true_bb();
+                            res = self.dfg_mut().replace_value_with(user.clone()).branch(
+                                replacer.clone(),
+                                bbt,
+                                bbf,
+                            );
+                        }
+                        ValueKind::Call(ca) => {
+                            let args = ca.args();
+                            let callee = ca.callee();
+                            let vec = args
+                                .iter()
+                                .map(|v| {
+                                    if v.clone() == inst.clone() {
+                                        replacer.clone()
+                                    } else {
+                                        v.clone()
+                                    }
+                                })
+                                .collect();
+                            res = self
+                                .dfg_mut()
+                                .replace_value_with(user.clone())
+                                .call(callee, vec);
+                            // panic!("exit here");
+                        }
+                        ValueKind::GetElemPtr(va) => {
+                            let mut lhs = va.src();
+                            let mut rhs = va.index();
+                            if lhs == inst.clone() {
+                                lhs = replacer.clone();
+                            }
+                            if rhs == inst.clone() {
+                                rhs = replacer.clone();
+                            }
+                            res = self
+                                .dfg_mut()
+                                .replace_value_with(user.clone())
+                                .get_elem_ptr(lhs, rhs);
+                        }
+                        ValueKind::GetPtr(va) => {
+                            let mut lhs = va.src();
+                            let mut rhs = va.index();
+                            if lhs == inst.clone() {
+                                lhs = replacer.clone();
+                            }
+                            if rhs == inst.clone() {
+                                rhs = replacer.clone();
+                            }
+                            res = self
+                                .dfg_mut()
+                                .replace_value_with(user.clone())
+                                .get_ptr(lhs, rhs);
+                        }
+                        ValueKind::Load(_) => {
+                            res = self
+                                .dfg_mut()
+                                .replace_value_with(user.clone())
+                                .load(replacer.clone());
+                        }
+                        ValueKind::Return(_) => {
+                            res = self
+                                .dfg_mut()
+                                .replace_value_with(user.clone())
+                                .ret(Some(replacer.clone()));
+                        }
+                        ValueKind::Store(st) => {
+                            let mut lhs = st.value();
+                            let mut rhs = st.dest();
+                            if lhs == inst.clone() {
+                                lhs = replacer.clone();
+                            }
+                            if rhs == inst.clone() {
+                                rhs = replacer.clone();
+                            }
+                            res = self
+                                .dfg_mut()
+                                .replace_value_with(user.clone())
+                                .store(lhs, rhs);
+                        }
+                        _ => {
+                            unreachable!("unexpected kind");
+                        }
+                    }
+                }
+                let len = self.dfg().value(inst.clone()).used_by().len();
+                if len == 0 {
+                    self.layout_mut()
+                        .bb_mut(blockval.bb)
+                        .insts_mut()
+                        .remove(inst);
+                    let _ = self.dfg_mut().remove_value(inst.clone());
                 }
             }
         }
